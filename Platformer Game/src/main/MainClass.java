a14fe74f564f48899060b300f08e9bccd8ed38cc
Mazuzu33 <mazuki.wu@gmail.com>
2023-05-14 20:50:31 -0500

    Ok

--- Platformer Game/src/main/MainClass.java
+++ Platformer Game/src/main/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("I am here in the world today");
-		System.out.print("what a haa");
+		System.out.print("what a haaaaa");
 	}
 
 }

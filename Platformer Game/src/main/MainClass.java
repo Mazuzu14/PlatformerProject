0c0f5fb851a747edba3f28734410e278ac45ca66
Mazuzu33 <mazuki.wu@gmail.com>
2023-05-14 20:47:31 -0500

    This is a test

--- Platformer Game/src/main/MainClass.java
+++ Platformer Game/src/main/MainClass.java
@@ -3,8 +3,8 @@ package main;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("I am");
-
+		System.out.println("I am here in the world today");
+		System.out.print("what");
 	}
 
 }
